5bc389c3a5f19670e308336190a689a593b6d157
Darko Pranjic <darkopranjic.4@gmail.com>
2017-12-20 13:11:41 +0100

    Fibonacci with iteration

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -2,5 +2,5 @@ function fibonacci(n) {
     if(n < 2) return n;
     return fibonacci(n-1) + fibonacci(n-2)
 }
-
+//https://technobeans.com/2012/04/16/5-ways-of-fibonacci-in-python/
 console.log(fibonacci(10))
---
+++ recursion/fibonacci_iteration.js
@@ -0,0 +1,11 @@
+function fibo(n) {
+    let [a,b] = [0,1];
+
+    for(let i = 0; i < n; i++){
+        [a, b] = [b, a + b];
+    }
+
+    return a;
+}
+
+console.log(fibo(10))
