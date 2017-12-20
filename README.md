307488d225b54250740cbee1d75bde29fe5a4d7a
Darko Pranjic <darkopranjic.4@gmail.com>
2017-12-20 10:52:09 +0100

    Added recursion permutation

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-<h1>Going through Python course, later solving tasks with Javascript</h1>
+<h1>Going through Python course of algorithms, later solving tasks with Javascript</h1>
---
+++ recursion/permutation.js
@@ -0,0 +1,25 @@
+function permute(s) {
+    
+    let out = []
+    if(s.length == 1){
+        out = [s]
+    }
+    else{
+
+        // For every letter in word
+        for(let i = 0; i < s.length; i++){ 
+
+            // Permute next word (ex. 1st is abc, we are sending bc into recursive call)
+            // For every returned combination push into out array
+            for(perm of permute( s.substring(0, i) + s.substring( i+1, s.length) )){
+                out.push(s[i] + perm)
+            }
+        }
+
+    }
+
+    return out
+
+}
+
+console.log(permute("abc"))
