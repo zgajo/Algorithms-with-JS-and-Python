c4d45d3e48f480917ea277baccc4aaefe7cc2c98
Darko Pranjic <dpranjic@iolap.com>
2021-09-23 13:57:08 +0200

    Store count of links to node

--- Trees/rtree-bush-clone/test.ts
+++ Trees/rtree-bush-clone/test.ts
@@ -7,19 +7,37 @@ import BTree from "./Btree";
 
 const btree = new BTree();
 const bTreeCity = new BTree();
+const bTreeNode = new BTree();
+const bTreeStreet = new BTree();
+const bTreeAddress = new BTree();
 const rtree = new RTree(10);
 
 const main = () => {
   // console.log(rtree.search(new Node(42.5059199, 1.5289214)));
   // console.log("End");
   // console.log(btree);
-  console.log(bTreeCity.filter((key) => key.startsWith("Me")).valuesArray());
+  /**
+   * Assuming you want only roads, then a possible algorithm is this:
+
+1. parse all ways; throw away those that are not roads, and for the others, remember the node IDs they consist of, by incrementing a "link counter" for each node referenced.
+2. parse all ways a second time; a way will normally become one edge, but if any nodes apart from the first and the last have a link counter greater than one, then split the way into two edges at that point. Nodes with a link counter of one and which are neither first nor last can be thrown away unless you need to compute the length of the edge.
+3. (if you need geometry for your graph nodes) parse the nodes section of the XML now, recording coordinates for all nodes that you have retained.
+If you are only working on a small data set you can of course simply read everything into memory and do the above analysis in memory.
+   */
+  console.log(
+    bTreeAddress.filter((key) => key.startsWith("Vicani")).valuesArray()
+  );
+
+  console.log(bTreeNode.get("51552755"));
 };
+console.time("test");
 
 parse({
   filePath: "andorra-latest.osm.pbf",
   endDocument: function () {
     // console.log(rtree);
+    console.timeEnd("test");
+    console.log("document end");
     main();
     // Najbolje je nakon inserta svih nodeova i waysa u Btree, proci kroz sve
   },
@@ -28,22 +46,85 @@ parse({
   },
   node: function (node: any) {
     // Spremiti node po id-ju u BTREE
-    if (node.tags["addr:city"]) {
+    // if (node.tags["addr:city"]) {
+    //   // Spremiti gradove po imenima u BTree
+    //   rtree.insert(new Node(node.lat, node.lon, node.tags));
+    // }
+    if (node.tags["addr:housenumber"] && node.tags["addr:street"]) {
       // Spremiti gradove po imenima u BTree
-      rtree.insert(new Node(node.lat, node.lon, node.tags));
+      if (node.tags.name) {
+        bTreeAddress.set(node.tags.name, node);
+      }
+
+      if (node.tags["addr:street"] && node.tags["addr:housenumber"]) {
+        bTreeAddress.set(
+          `${node.tags["addr:street"]} ${node.tags["addr:housenumber"]}`,
+          node
+        );
+      }
     }
 
-    if (node.tags.place) {
-      // console.log(node);
-      bTreeCity.set(node.tags.name, node);
-    }
+    // if (node.tags.place) {
+    //   // console.log(node);
+    //   bTreeCity.set(node.tags.name, node);
+    // }
     // if (node.id === "6393274537") {
     //   console.log("NODE: ", node);
     // }
   },
   way: function (way: any) {
+    if (way.tags.highway === "residential") {
+      // console.log(node);
+      if (way.tags.name) {
+        bTreeAddress.set(`${way.tags.name}|${way.id}`, way);
+      }
+    }
+
+    if (way.tags["addr:street"]) {
+      // Spremiti gradove po imenima u BTree
+      if (way.tags.name) {
+        bTreeAddress.set(way.tags.name, way);
+      }
+
+      if (way.tags["addr:housenumber"]) {
+        return bTreeAddress.set(
+          `${way.tags["addr:street"]} ${way.tags["addr:housenumber"]}`,
+          way
+        );
+      }
+
+      bTreeAddress.set(`${way.tags["addr:street"]}}`, way);
+    }
+
+    if (way.tags.building && way.tags.name) {
+      // Spremiti gradove po imenima u BTree
+      if (way.tags.name) {
+        bTreeAddress.set(way.tags.name, way);
+      }
+
+      if (way.tags["addr:street"] && way.tags["addr:housenumber"]) {
+        bTreeAddress.set(
+          `${way.tags["addr:street"]} ${way.tags["addr:housenumber"]}`,
+          way
+        );
+      }
+    }
+
     if (way.tags.highway) {
-      btree.set(way.id, way);
+      way.nodeRefs.forEach((element: string) => {
+        const node = bTreeNode.get(element);
+        if (node) {
+          node.linkCount + 1;
+          node.partOfWays.push(way);
+        } else {
+          bTreeNode.set(element, {
+            id: element,
+            linkCount: 1,
+            partOfWays: [way],
+          });
+        }
+      });
+      // bTreeNode.set(way.id, way);
       // Spremiti ulice po imenima u BTree
       // console.log("way", way);
       // if (way.tags?.name?.includes("Carrer Pau Casals")) {
